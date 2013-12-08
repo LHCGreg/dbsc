5edf375d6348d81078db77f63f25931584ee1b87
Greg Najda <LHCGreg@server.fake>
2013-12-07 23:51:05 -0500

    Integration tests for mongodbsc update.

---
+++ mongodbsc.Integration/BaseTestFixture.cs
@@ -0,0 +1,241 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace dbsc.Mongo.Integration
+{
+    [TestFixture]
+    abstract class BaseTestFixture
+    {
+        protected static readonly string TestDatabaseName = "mongodbsc_test";
+        protected static readonly string AltTestDatabaseName = "mongodbsc_test_2";
+        protected static readonly string SourceDatabaseName = "mongodbsc_test_source";
+        protected static readonly string CreationTemplateDatabaseName = "creation_template_test";
+
+        protected string MongodbscPath { get; private set; }
+        protected string ScriptsDir { get; private set; }
+
+        [TestFixtureSetUp]
+        public void SetDirectories()
+        {
+            Uri thisAssemblyUri = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            string thisAssemblyPath = thisAssemblyUri.LocalPath;
+            string thisAssemblyDir = Path.GetDirectoryName(thisAssemblyPath);
+            MongodbscPath = Path.Combine(thisAssemblyDir, "mongodbsc.exe");
+            ScriptsDir = Path.Combine(thisAssemblyDir, "scripts");
+        }
+
+        protected List<Book> ExpectedBooks = new List<Book>()
+        {
+            new Book()
+            {
+                name = "A Game of Thrones",
+                author = "George R.R. Martin"
+            },
+            new Book()
+            {
+                name = "Clean Code",
+			    author = "Robert C. Martin"
+            },
+            new Book()
+            {
+                name = "The Mythical Man-Month",
+			    author = "Frederick P. Brooks, Jr."
+            }
+        };
+
+        protected List<Book> ExpectedSourceBooks = new List<Book>()
+        {
+            new Book()
+            {
+                name = "Charlie and the Chocolate Factory",
+                author = "Roald Dahl"
+            }
+        };
+
+        protected List<Person> ExpectedPeople = new List<Person>()
+        {
+            new Person()
+            {
+                name = "Greg",
+                preferences = new PersonPreferences()
+                {
+                    a = 500,
+                    b = new List<int>() { 800, 900 },
+                    c = true
+                }
+            },
+            new Person()
+            {
+                name = "Joe",
+                preferences = new PersonPreferences()
+                {
+                    a = 1000,
+                    b = null,
+                    c = false
+                }
+            }
+        };
+
+        protected List<Number> ExpectedNumbers = new List<Number>()
+        {
+            new Number()
+            {
+                num = 1,
+                english = "one",
+                spanish = "uno"
+            },
+            new Number()
+            {
+                num = 2,
+                english = "two",
+                spanish = "dos"
+            }
+        };
+
+        protected void DropDatabase(string dbName)
+        {
+            MongoClient mongoClient = new MongoClient("mongodb://localhost");
+            MongoServer server = mongoClient.GetServer();
+            if (server.DatabaseExists(dbName))
+            {
+                server.DropDatabase(dbName);
+            }
+        }
+
+        private string GetAuthServerConnectionString()
+        {
+            MongoConnectionStringBuilder builder = new MongoConnectionStringBuilder();
+            builder.Server = new MongoServerAddress("localhost", 30017);
+            builder.Username = "useradmin";
+            builder.Password = "testpw";
+            return builder.ToString();
+        }
+
+        protected void DropDatabaseOnAuthMongo(string dbName)
+        {
+            string connectionString = GetAuthServerConnectionString();
+            MongoClient mongoClient = new MongoClient(connectionString);
+            MongoServer server = mongoClient.GetServer();
+            if (server.DatabaseExists(dbName))
+            {
+                server.DropDatabase(dbName);
+            }
+        }
+
+        protected int RunCommand(string arguments)
+        {
+            using (Process mongodbsc = new Process())
+            {
+                mongodbsc.StartInfo.Arguments = arguments;
+                mongodbsc.StartInfo.FileName = MongodbscPath;
+                mongodbsc.StartInfo.RedirectStandardOutput = true;
+                mongodbsc.StartInfo.RedirectStandardError = true;
+                mongodbsc.StartInfo.UseShellExecute = false;
+                mongodbsc.StartInfo.WorkingDirectory = ScriptsDir;
+                mongodbsc.StartInfo.CreateNoWindow = true;
+
+                mongodbsc.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                mongodbsc.ErrorDataReceived += (sender, e) => Console.WriteLine(e.Data);
+
+                mongodbsc.Start();
+                mongodbsc.BeginOutputReadLine();
+                mongodbsc.BeginErrorReadLine();
+                mongodbsc.WaitForExit();
+
+                return mongodbsc.ExitCode;
+            }
+        }
+
+        protected void RunSuccesfulCommand(string arguments)
+        {
+            int returnCode = RunCommand(arguments);
+            Assert.That(returnCode, Is.EqualTo(0));
+        }
+
+        protected void RunUnsuccessfulCommand(string arguments)
+        {
+            int returnCode = RunCommand(arguments);
+            Assert.That(returnCode, Is.Not.EqualTo(0));
+        }
+
+        protected void VerifyDatabaseOnAuthMongo(string databaseName, List<Book> expectedBooks, List<Person> expectedPeople, List<Number> expectedNumbers)
+        {
+            string connectionString = GetAuthServerConnectionString();
+            VerifyDatabase(connectionString, databaseName, expectedBooks, expectedPeople, expectedNumbers);
+        }
+
+        protected void VerifyDatabase(string databaseName, List<Book> expectedBooks, List<Person> expectedPeople, List<Number> expectedNumbers)
+        {
+            VerifyDatabase("mongodb://localhost", databaseName, expectedBooks, expectedPeople, expectedNumbers);
+        }
+
+        protected void VerifyDatabase(string connectionString, string databaseName, List<Book> expectedBooks, List<Person> expectedPeople, List<Number> expectedNumbers)
+        {
+            MongoClient mongoClient = new MongoClient(connectionString);
+            MongoServer server = mongoClient.GetServer();
+            MongoDatabase database = server.GetDatabase(databaseName);
+
+            MongoCollection<Book> bookCollection = database.GetCollection<Book>("books");
+            List<Book> books = bookCollection.FindAll().ToList();
+
+            Assert.That(books, Is.EquivalentTo(expectedBooks));
+
+            GetIndexesResult indexes = bookCollection.GetIndexes();
+            BsonDocument expectedIndexKey = new BsonDocument();
+            expectedIndexKey["name"] = 1;
+            Assert.That(indexes.Any(index => index.Key == expectedIndexKey));
+
+            MongoCollection<Person> personCollection = database.GetCollection<Person>("people");
+            List<Person> people = personCollection.FindAll().ToList();
+
+            Assert.That(people, Is.EquivalentTo(expectedPeople));
+
+            MongoCollection<Number> numberCollection = database.GetCollection<Number>("numbers");
+            List<Number> numbers = numberCollection.FindAll().ToList();
+
+            Assert.That(numbers, Is.EquivalentTo(expectedNumbers));
+        }
+
+        protected void VerifyCreationTemplateDatabase()
+        {
+            MongoClient mongoClient = new MongoClient("mongodb://localhost");
+            MongoServer server = mongoClient.GetServer();
+            MongoDatabase database = server.GetDatabase(CreationTemplateDatabaseName);
+            MongoCollection<TestCollectionObject> testCollection = database.GetCollection<TestCollectionObject>("testCollection");
+            Assert.That(testCollection.FindAll().First().pass, Is.True);
+        }
+
+        protected void VerifyCreationTemplateDatabaseOnAuthMongo()
+        {
+            MongoClient mongoClient = new MongoClient("mongodb://useradmin:testpw@localhost:30017");
+            MongoServer server = mongoClient.GetServer();
+            MongoDatabase database = server.GetDatabase(CreationTemplateDatabaseName);
+            MongoCollection<TestCollectionObject> testCollection = database.GetCollection<TestCollectionObject>("testCollection");
+            Assert.That(testCollection.FindAll().First().pass, Is.True);
+        }
+    }
+}
+
+/*
+ Copyright 2013 Greg Najda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
---
+++ mongodbsc.Integration/UpdateTestFixture.cs
@@ -0,0 +1,181 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dbsc.Mongo.Integration
+{
+    [TestFixture]
+    class UpdateTestFixture : BaseTestFixture
+    {
+        [Test]
+        public void BasicTest()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand("update");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void BasicTestWithAuth()
+        {
+            DropDatabaseOnAuthMongo(TestDatabaseName);
+            CheckoutZeroOnAuthMongo();
+            RunSuccesfulCommand("update -port 30017 -u useradmin -p testpw");
+            VerifyDatabaseOnAuthMongo(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void PartialUpdateTest()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand("update -r 1");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, new List<Number>());
+        }
+
+        [Test]
+        public void UpdateNothingTest()
+        {
+            DropDatabase(TestDatabaseName);
+            RunSuccesfulCommand("checkout");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+            RunSuccesfulCommand("update");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void UpdateNothingTestWithExplicitRevision()
+        {
+            DropDatabase(TestDatabaseName);
+            RunSuccesfulCommand("checkout");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+            RunSuccesfulCommand("update -r 2");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void TestPartialUpdateWithTooHighRevision()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunUnsuccessfulCommand("update -r 3");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, new List<Person>(), new List<Number>());
+        }
+
+        [Test]
+        public void BasicImportTest()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand(string.Format("update -targetDb {0} -sourceDbServer localhost -sourceDb {1}", TestDatabaseName, SourceDatabaseName));
+            VerifyDatabase(TestDatabaseName, ExpectedSourceBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void BasicImportTestWithAuth()
+        {
+            DropDatabaseOnAuthMongo(TestDatabaseName);
+            CheckoutZeroOnAuthMongo();
+            RunSuccesfulCommand(string.Format("update -port 30017 -u useradmin -p testpw -sourceDbServer localhost -sourceDb {0}", SourceDatabaseName));
+            VerifyDatabaseOnAuthMongo(TestDatabaseName, ExpectedSourceBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void ImportOnlyOneCollectionTest()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand(string.Format("update -sourceDbServer localhost -sourceDb {0} -importTableList tables_to_import.txt", SourceDatabaseName));
+            VerifyDatabase(TestDatabaseName, ExpectedSourceBooks, new List<Person>(), new List<Number>());
+        }
+
+        [Test]
+        public void ImportOnlyOneCollectionWithAuthTest()
+        {
+            DropDatabaseOnAuthMongo(TestDatabaseName);
+            CheckoutZeroOnAuthMongo();
+            RunSuccesfulCommand(string.Format("update -port 30017 -u useradmin -p testpw -sourceDbServer localhost -sourceDb {0} -importTableList tables_to_import.txt", SourceDatabaseName));
+            VerifyDatabaseOnAuthMongo(TestDatabaseName, ExpectedSourceBooks, new List<Person>(), new List<Number>());
+        }
+
+        [Test]
+        public void TestTargetDb()
+        {
+            DropDatabase(TestDatabaseName);
+            DropDatabase(AltTestDatabaseName);
+
+            // First get the source database into the the main test database
+            RunSuccesfulCommand(string.Format("checkout -targetDb {0} -sourceDbServer localhost -sourceDb {1}", TestDatabaseName, SourceDatabaseName));
+
+            // Then import from the main test database into the alt test database
+            CheckoutZeroOnAltDatabase();
+            RunSuccesfulCommand(string.Format("update -targetDbServer localhost -targetDb {0} -port 27017 -sourceDbServer localhost -sourcePort 27017", AltTestDatabaseName));
+            VerifyDatabase(AltTestDatabaseName, ExpectedSourceBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void TestErrorInScriptAborts()
+        {
+            DropDatabase(TestDatabaseName);
+            RunSuccesfulCommand("checkout -r 0 -dir ../error_test_scripts");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, new List<Person>(), new List<Number>());
+            RunUnsuccessfulCommand("update -dir ../error_test_scripts");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, expectedNumbers: new List<Number>());
+        }
+
+        [Test]
+        public void TestPartialUpdateWithImport()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand(string.Format("update -r 2 -sourceDbServer localhost -sourceDb {0}", SourceDatabaseName));
+            VerifyDatabase(TestDatabaseName, ExpectedSourceBooks, ExpectedPeople, ExpectedNumbers);
+        }
+
+        [Test]
+        public void TestPartialUpdateShortOfImport()
+        {
+            DropDatabase(TestDatabaseName);
+            CheckoutZero();
+            RunSuccesfulCommand(string.Format("update -r 1 -sourceDbServer localhost -sourceDb {0}", SourceDatabaseName));
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, ExpectedPeople, expectedNumbers: new List<Number>());
+        }
+
+        private void CheckoutZero()
+        {
+            RunSuccesfulCommand("checkout -r 0");
+            VerifyDatabase(TestDatabaseName, ExpectedBooks, new List<Person>(), new List<Number>());
+        }
+
+        private void CheckoutZeroOnAuthMongo()
+        {
+            RunSuccesfulCommand("checkout -port 30017 -u useradmin -p testpw -r 0");
+            VerifyDatabaseOnAuthMongo(TestDatabaseName, ExpectedBooks, new List<Person>(), new List<Number>());
+        }
+
+        private void CheckoutZeroOnAltDatabase()
+        {
+            RunSuccesfulCommand(string.Format("checkout -r 0 -targetDb {0}", AltTestDatabaseName));
+            VerifyDatabase(AltTestDatabaseName, ExpectedBooks, new List<Person>(), new List<Number>());
+        }
+    }
+}
+
+/*
+ Copyright 2013 Greg Najda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
