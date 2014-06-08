9033097e203658bc55ababce72743e75402dd07e
Greg Najda <gregnajda@gmail.com>
2014-06-07 23:48:23 -0400

    Fix platform and bin folders in global run_tests scripts.

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
-xbuild /t:Build "/p:Configuration=Debug;Platform=x86" mongodbsc.Integration/mongodbsc.Integration.csproj
-xbuild /t:Build "/p:Configuration=Debug;Platform=x86" pgdbsc.Integration/pgdbsc.Integration.csproj
-xbuild /t:Build "/p:Configuration=Debug;Platform=x86" mydbsc.Integration/mydbsc.Integration.csproj
+xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" mongodbsc.Integration/mongodbsc.Integration.csproj
+xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" pgdbsc.Integration/pgdbsc.Integration.csproj
+xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" mydbsc.Integration/mydbsc.Integration.csproj
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" dbsc.Core.Tests/dbsc.Core.Tests.csproj
 
-nunit-console "dbsc.Core.Tests/bin/Debug/dbsc.Core.Tests.dll" "mongodbsc.Integration/bin/Debug_x86/mongodbsc.Integration.dll" "pgdbsc.Integration/bin/Debug_x86/pgdbsc.Integration.dll" "mydbsc.Integration/bin/Debug_x86/mydbsc.Integration.dll" -nologo -noxml "-out=test_stdout.txt" "-err=test_stderr.txt"
+nunit-console "dbsc.Core.Tests/bin/Debug_AnyCPU/dbsc.Core.Tests.dll" "mongodbsc.Integration/bin/Debug_AnyCPU/mongodbsc.Integration.dll" "pgdbsc.Integration/bin/Debug_AnyCPU/pgdbsc.Integration.dll" "mydbsc.Integration/bin/Debug_AnyCPU/mydbsc.Integration.dll" -nologo -noxml "-out=test_stdout.txt" "-err=test_stderr.txt"
