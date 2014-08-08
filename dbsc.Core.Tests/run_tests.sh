41639f6eb9b6cfdd12a3a7d48efe4551356ed7e3
Greg Najda <gregnajda@gmail.com>
2014-08-08 17:38:12 -0400

    Fix dbsc.Core.Tests run_tests scripts.

--- dbsc.Core.Tests/run_tests.sh
+++ dbsc.Core.Tests/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" dbsc.Core.Tests.csproj
-nunit-console "bin/Debug/dbsc.Core.Tests.dll" -nologo -noxml "-out=bin/Debug_AnyCPU/test_stdout.txt" "-err=bin/Debug/test_stderr.txt"
+nunit-console "bin/Debug_AnyCPU/dbsc.Core.Tests.dll" -nologo -noxml "-out=bin/Debug_AnyCPU/test_stdout.txt" "-err=bin/Debug_AnyCPU/test_stderr.txt"
