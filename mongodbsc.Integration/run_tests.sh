c6ae999f3391647ec79fa55efdbae806e4a379a4
Greg Najda <gregnajda@gmail.com>
2014-08-30 21:35:42 -0400

    Fix typo in mongodbsc run_tests.sh.

--- mongodbsc.Integration/run_tests.sh
+++ mongodbsc.Integration/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" mongodbsc.Integration.csproj
-nunit-console "bin/Debug_AnyCPU/mongodbsc.Integration.dll" -nologo -noxml "-out=bin/Debug_AnyCPU/test_stdout.txt" "-err=bin/DebugAnyCPU/test_stderr.txt"
+nunit-console "bin/Debug_AnyCPU/mongodbsc.Integration.dll" -nologo -noxml "-out=bin/Debug_AnyCPU/test_stdout.txt" "-err=bin/Debug_AnyCPU/test_stderr.txt"
