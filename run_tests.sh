3b43c117eab3d9cbdca2623e264b99139788bedf
Greg Najda <gregnajda@gmail.com>
2014-09-15 21:54:28 -0400

    Fix build configs in global run_tests scripts.

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,8 @@ set -e
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" mongodbsc.Integration/mongodbsc.Integration.csproj
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" pgdbsc.Integration/pgdbsc.Integration.csproj
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" mydbsc.Integration/mydbsc.Integration.csproj
-xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" oradbsc.Integration/oradbsc.Integration.csproj
+xbuild /t:Build "/p:Configuration=DebugMono;Platform=AnyCPU" oradbsc.Integration/oradbsc.Integration.csproj
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" dbsc.Core.Tests/dbsc.Core.Tests.csproj
 xbuild /t:Build "/p:Configuration=Debug;Platform=AnyCPU" dbsc.Core.Antlr.Tests/dbsc.Core.Antlr.Tests.csproj
 
-nunit-console "dbsc.Core.Tests/bin/Debug_AnyCPU/dbsc.Core.Tests.dll" "dbsc.Core.Antlr.Tests/bin/Debug_AnyCPU/dbsc.Core.Antlr.Tests.dll" "mongodbsc.Integration/bin/Debug_AnyCPU/mongodbsc.Integration.dll" "pgdbsc.Integration/bin/Debug_AnyCPU/pgdbsc.Integration.dll" "mydbsc.Integration/bin/Debug_AnyCPU/mydbsc.Integration.dll" "oradbsc.Integration/bin/Debug_AnyCPU/oradbsc.Integration.dll" -nologo -noxml "-out=test_stdout.txt" "-err=test_stderr.txt"
+nunit-console "dbsc.Core.Tests/bin/Debug_AnyCPU/dbsc.Core.Tests.dll" "dbsc.Core.Antlr.Tests/bin/Debug_AnyCPU/dbsc.Core.Antlr.Tests.dll" "mongodbsc.Integration/bin/Debug_AnyCPU/mongodbsc.Integration.dll" "pgdbsc.Integration/bin/Debug_AnyCPU/pgdbsc.Integration.dll" "mydbsc.Integration/bin/Debug_AnyCPU/mydbsc.Integration.dll" "oradbsc.Integration/bin/DebugMono_AnyCPU/oradbsc.Integration.dll" -nologo -noxml "-out=test_stdout.txt" "-err=test_stderr.txt"
